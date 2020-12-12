34bb3f57bfa356ab76f6ab430095d278e0b54e66
Frédéric Wang <fwang@igalia.com>
2020-12-12 10:31:04 +0100

    Use public protocol handler page provided by @lidel instead of a local one.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Demo : Extension to register a local HTTP gateway for IPFS
+# Demo : Extension to register a HTTP gateway for IPFS
 
 This is a small extension allowing to register a
-[local HTTP gateway](https://docs.ipfs.io/concepts/ipfs-gateway/#gateway-providers)
+[HTTP gateway](https://docs.ipfs.io/concepts/ipfs-gateway/#gateway-providers)
 for IPFS using `registerProtocolHandler()`. This is just a proof of concept for
 [bug 64100](https://bugs.chromium.org/p/chromium/issues/detail?id=64100),
 the UI can be refined (see [bug 1079333](https://bugs.chromium.org/p/chromium/issues/detail?id=1079333) for example)
--- register.js
+++ register.js
@@ -1,6 +1,10 @@
+// This handler performs an extra redirect that can eventually be
+// removed. See https://github.com/ipfs/go-ipfs/issues/7686
+const handler_url = "https://bafkreihnpo5zt7qt3lvmk4zk2opran7ybbe3gfp5ruqf5iirdyxaxqvwtu.ipfs.dweb.link/#%s";
+
 window.addEventListener("DOMContentLoaded", function() {
     document.querySelector("button").addEventListener("click", function() {
-        navigator.registerProtocolHandler("ipfs", "./handler.html?link=%s",
+        navigator.registerProtocolHandler("ipfs", handler_url,
                                           "dummy_title_for_backward_compat");
     });
 });
